d1a61b4a59531c6bc3a8ddd1665b0ac7ed57fa36
jayrsawal <jayrsawal@gmail.com>
2019-04-25 18:10:29 -0400

    Similar word modal - adding bulk signals next

--- public/js/action.js
+++ public/js/action.js
@@ -15,8 +15,52 @@ function findSimilar(elem) {
   let $elem = $(elem).parent();
   let signal = $("#signal", $elem).html();
   getSimilarWords(signal, function(results) {
-    console.log(results);
-    $("#generic-modal").modal("show");
+    let $modal = $("#generic-modal");
+    $(".modal-title", $modal).html("Similar words to \"" + signal + "\" by usage");
+    $(".modal-body").html("");
+
+    let $body = $(".modal-body", $modal);
+    $body.html("");
+
+    let $tbody = $("<tbody></tbody>");
+    for(let i=0; i<results.length; i++) {
+      let o = results[i];
+      let html = "<tr><td><div class='input-group-text'>\
+        <input type='checkbox' class='similar-word' data-word='" + o.word
+        + "'></div></td><td>" + o.word + "</td><td>" + (o.score*100).toFixed(2)
+        + "</td></tr>";
+      $tbody.append(html);
+    }
+
+    let $table = $(`<table class='table'>
+      <thead>
+        <tr>
+          <th>
+            <div class="input-group-text">
+              <input type="checkbox" id="select-all-similar">
+            </div>
+          </th>
+          <th>Word</th>
+          <th>Similarity (%)</th>
+        </tr>
+      </thead>
+    </table>`);
+    $table.append($tbody);
+    $body.append($table);
+
+    $("#select-all-similar").change(function() {
+      $(".similar-word").prop("checked", this.checked);
+    })
+
+    $(".modal-footer").html("");
+    $(".modal-footer").append("<button class='btn btn-primary' \
+      onclick='addSimilarSignals(1);'>Add as Signal</button>");
+    $(".modal-footer").append("<button class='btn btn-primary' \
+      onclick='addSimilarSignals(2);'>Add as Filter</button>");
+    $(".modal-footer").append("<button class='btn btn-primary' \
+      onclick='addSimilarSignals(3);'>Add as Restriction</button>");
+
+    $modal.modal("show");
   });
 }
 
@@ -39,4 +83,8 @@ function findPrediction(elem) {
   getPrediction(signal, function(results) {
     console.log(results);
   });
+}
+
+function addSimilarSignals(type) {
+  console.log($(".similar-word:checked"));
 }
