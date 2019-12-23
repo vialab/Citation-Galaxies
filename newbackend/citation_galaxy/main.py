eacda8a7b3e77c2f9d4ce8791a62fd349d0d8601
Nathan Beals <nathan.beals@ontariotechu.net>
2019-12-22 23:49:54 -0500

    Started remaking main server component

---
+++ newbackend/citation_galaxy/main.py
@@ -0,0 +1,58 @@
+import uvloop
+uvloop.install()
+# asyncio.set_event_loop_policy(uvloop.get_event_loop_policy())
+
+import logging
+import sys
+
+import aiohttp_jinja2
+import jinja2
+from aiohttp import web
+
+# from citation_galaxy.db import close_pg, init_pg
+from citation_galaxy.middlewares import setup_middlewares
+from citation_galaxy.routes import setup_routes
+from citation_galaxy.settings import get_config
+
+import asyncpg
+
+
+async def init_app(argv=None):
+
+    app = web.Application()
+
+    app['config'] = get_config(argv)
+
+    print(app['config']['postgres'])
+
+    app['db'] = await asyncpg.create_pool(**app['config']['postgres'])
+
+    # setup Jinja2 template renderer
+    aiohttp_jinja2.setup(
+        app, loader=jinja2.PackageLoader('citation_galaxy', 'templates'))
+
+    # create db connection on startup, shutdown on exit
+    # app.on_startup.append(init_pg)
+    # app.on_cleanup.append(close_pg)
+
+    # setup views and routes
+    setup_routes(app)
+
+    setup_middlewares(app)
+
+    return app
+
+
+def main(argv):
+    logging.basicConfig(level=logging.DEBUG)
+
+    app = init_app(argv)
+
+    config = get_config(argv)
+    web.run_app(app,
+                host=config['host'],
+                port=config['port'])
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ newbackend/citation_galaxy/utils.py
@@ -0,0 +1,23 @@
+# utils.py
+import trafaret as T
+
+
+TRAFARET = T.Dict({
+    T.Key('postgres'):
+        T.Dict({
+            'database': T.String(),
+            'user': T.String(),
+            'password': T.String(),
+            'host': T.String(),
+            'port': T.Int(),
+            'min_size': T.Int(),
+            'max_size': T.Int(),
+        }),
+    T.Key('host'): T.IP,
+    T.Key('port'): T.Int(),
+    T.Key('year_range'):
+        T.Dict({
+            'min': T.Int(),
+            'max': T.Int()
+        }),
+})
