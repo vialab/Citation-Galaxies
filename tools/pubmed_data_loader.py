425193c70680ecd70be161cf87339360934150c0
zac-hills <codinfreakgf@gmail.com>
2020-08-31 10:50:43 -0400

    added cli params for pubmed_data_loader

--- tools/pubmed_data_loader.py
+++ tools/pubmed_data_loader.py
@@ -7,6 +7,7 @@ import asyncpg
 import json
 import smtplib
 import yagmail
+import sys
 
 def load_directory(directory: str):
     path_all = pp.list_xml_path(directory)
@@ -185,8 +186,11 @@ async def create_word_map():
     return
     
 async def main():
+    if len(sys.argv) < 2:
+        print("Requires a path name to be passed in as a second paramater")
+        exit(-1)
     conn = await asyncpg.connect('postgresql://citationdb:citationdb@localhost:5435/citationdb')
-    paths = load_directory('/scratch/zhills/PubMed/non_commercial')
+    paths = load_directory(sys.argv[1])
     end = len(paths)
     counter=0
     for path in paths:
